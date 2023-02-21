bba57b0f509aeb72823c6b4cba3c2c608a1a7210
Wilmer S Zuna Largo <zunalargowilmer@gmail.com>
2023-02-21 00:46:50 -0800

    [camera-rays][bug] Implement camera ray projection

--- src/student/camera.cpp
+++ src/student/camera.cpp
@@ -2,6 +2,7 @@
 #include "../util/camera.h"
 #include "../rays/samplers.h"
 #include "debug.h"
+#include <math.h>
 
 Ray Camera::generate_ray(Vec2 screen_coord) const {
 
@@ -17,6 +18,21 @@ Ray Camera::generate_ray(Vec2 screen_coord) const {
     //
     // Tip: compute the ray direction in view space and use
     // the camera space to world space transform (iview) to transform the ray back into world space.
+    // the camera transform to transform it back into world space.
+    
+    // Create ray origin
+    Vec3 origin(position);
+    
+    // Create ray direction. Start with 2D
+    Vec2 pix_screen(2 * screen_coord.x - 1, 1 - 2 * screen_coord.y);
+    float fov_rads = vert_fov * M_PI / 180;
+    float tan_fov = tan(fov_rads / 2);
+    Vec3 pix_camera((2 * pix_screen.x - 1) * aspect_ratio * tan_fov, 
+                     (1 - 2 * pix_screen.y) * tan_fov, -1.0);
+    // Convert from view space to world-space. cf. iview
+    Vec3 dir(pix_camera - origin);
 
-    return Ray();
+    // Construct ray
+    Ray out(origin, dir);
+    return out;
 }
